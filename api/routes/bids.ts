51907b43ddba8b45d0b4c02fa7e21fe86317ce63
OPD Livmind <opd.livmind@gmail.com>
2026-05-30 16:25:50 +0100

    Apply requireVerifiedVendor middleware to bid creation

--- api/routes/bids.ts
+++ api/routes/bids.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { authenticate } from "../middleware/auth";
+import { authenticate, requireVerifiedVendor } from "../middleware/auth";
 import * as bidController from "../controllers/bidController";
 
 const router = Router();
 
 router.get("/", authenticate, bidController.listBids);
-router.post("/", authenticate, bidController.createBid);
+router.post("/", authenticate, requireVerifiedVendor, bidController.createBid);
 router.get("/:id", authenticate, bidController.getBid);
 router.patch("/:id/select", authenticate, bidController.selectBid);
 router.patch("/:id/reject", authenticate, bidController.rejectBid);
